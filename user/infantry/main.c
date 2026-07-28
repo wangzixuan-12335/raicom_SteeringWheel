55db04a2951618bbd5158fddb5aafc2a098a00df
LuboZhen <3383624911@qq.com>
2026-07-28 16:53:54 +0800

    fix: 原本的初始化，没弄龙门架和tof，tof文件之前调取有问题

--- user/infantry/main.c
+++ user/infantry/main.c
@@ -8,4 +8,51 @@
 #include "tasks.h"
 
 int main(void) {
+
+    //初始化Delay和I2C
+    My_delay_init();
+    MyI2C_Init();
+
+    //遥控器USART+DMA转运, 
+    //USART在中断里面处理解码，解码数据在结构体usart1_data_decoded中
+    MY_USART_Init();
+    MyDMA_Init((uint32_t)&(USART1->DR),(uint32_t)usart1_raw_data,18);
+	
+    //初始化夹爪舵机结构体，PWM初始化
+    //BSP_PWM_Init内TIM_OCMode_PWM2 修改为 TIM_OCMode_PWM1
+    BSP_PWM_Set_Port(&PWM_Holding_Jaw_Servo,PWM_PD12);
+    BSP_PWM_Init(&PWM_Holding_Jaw_Servo,1800,1000,TIM_OCPolarity_High);
+
+    //初始化底盘电机结构体
+    Motor_3508_Init(&Motor_3508_LF,IsPositive_True,19,&Motor_3508_LF_PID,&Motor_3508_LF_Position_PID);
+    Motor_3508_Init(&Motor_3508_RF,IsPositive_False,19,&Motor_3508_RF_PID,&Motor_3508_RF_Position_PID);
+    Motor_3508_Init(&Motor_3508_LB,IsPositive_True,19,&Motor_3508_LB_PID,&Motor_3508_LB_Position_PID);
+    Motor_3508_Init(&Motor_3508_RB,IsPositive_False,19,&Motor_3508_RB_PID,&Motor_3508_RB_Position_PID);
+
+    //初始化底盘MOTOR3508的pid
+    PID_Init(Motor_3508_LF.Motor_PID,7.5,0.1,0.5,10000,3000);
+    PID_Init(Motor_3508_RF.Motor_PID,7.5,0.1,0.5,10000,3000);
+    PID_Init(Motor_3508_LB.Motor_PID,7.5,0.1,0.5,10000,3000);
+    PID_Init(Motor_3508_RB.Motor_PID,7.5,0.1,0.5,10000,3000);
+
+    //底盘电机结构体合集
+    MyMotor_3508_Collection.LF=&Motor_3508_LF;
+    MyMotor_3508_Collection.RF=&Motor_3508_RF;
+    MyMotor_3508_Collection.LB=&Motor_3508_LB;
+    MyMotor_3508_Collection.RB=&Motor_3508_RB;
+
+    //初始化CAN通信
+    BSP_CAN_Init();
+
+    //初始化TIM2，生成一个1KHZ的中断，主要业务写在这个中断中，要保证所有对象初始化完毕，故应最后开启
+    //BSP_TIM2_Init经过修改，改了分频器，GPIOA初始化被注释
+    BSP_TIM2_Init();
+
+    //初始化TIM3，生成一个0.5KHZ的中断，保持电机和底盘角度写在这个中断中
+    //若写在TIM2中会导致CAN发送不稳，导致电机过冲
+    BSP_TIM3_Init();
+    while (1)
+    {
+    }
+    
 }
